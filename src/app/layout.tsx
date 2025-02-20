07c75968563a82b8aad6e18765f8a0408a2bde8b
Amelia Rose <nyameliaaaa@proton.me>
2025-02-20 20:37:10 +0300

    yea

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,5 @@
 import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
 
 export const metadata: Metadata = {
   title: "amelia rose :3",
@@ -26,6 +15,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="bg-base text-text">
         <div className="h-[100vh] w-[100vw] overflow-hidden bg-gradient-to-br from-mantle via-crust to-mantle">
+          {/* trans flag :3 */}
           <div className="mb-2">
             <div className="grid grid-cols-5">
               <div className="h-4 bg-sapphire"></div>
@@ -35,7 +25,25 @@ export default function RootLayout({
               <div className="h-4 bg-sapphire"></div>
             </div>
           </div>
-          <div className="overflow-y-scroll">{children}</div>
+
+          {/* content */}
+          <div className="overflow-y-scroll">
+            <div className="mx-auto md:max-w-7xl my-4 px-2 md:px-0">
+              {children}
+            </div>
+          </div>
+
+          {/* footer */}
+          <div className="mx-auto md:max-w-7xl px-2 md:px-0">
+            <div className="h-1 mb-3 bg-surface0"></div>
+            <p className="mb-1 text-subtext1 items-center">
+              &copy; copyright amelia rose {new Date().getFullYear()}{" "}
+              <span className="text-xs">(not rly but idfc)</span>
+            </p>
+            <p className="text-subtext1">
+              powered by nextjs + cloudflare pages
+            </p>
+          </div>
         </div>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,34 @@
+/* eslint-disable @next/next/no-img-element */
+
 export default function Home() {
   return (
     <div>
-      <p>hi :3</p>
+      <div className="mb-5">
+        {/* header :3 */}
+        <div className="flex flex-row items-center justify-center px-4 md:px-24">
+          <div className="w-1/3">
+            <img
+              alt=""
+              src="/images/amelia.png"
+              className="rounded-full mx-auto w-[8vw] h-[8vw]"
+            />
+          </div>
+          <div className="w-2/3">
+            <h1 className="text-pink font-bold text-2xl md:text-5xl">
+              amelia rose :3
+            </h1>
+            <p className="text-md text-subtext0 font-medium md:text-2xl">
+              nyamelia
+            </p>
+            <p className="mt-5 md:mt-7 text-md md:text-2xl font-medium">
+              hi :3
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <p>:3</p>
+      <p>run before i pound your mom  </p>
     </div>
   );
 }
