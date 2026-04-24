01debaeb0182e7f820d3680db5057d86add5550a
amelia-rose-deploy <nyameliaaaa@proton.me>
2026-04-24 19:55:07 +0300

    alright so its broken lol

---
+++ deploy.mjs
@@ -0,0 +1,13 @@
+// deploy.mjs
+import { execSync } from "child_process";
+
+const run = (cmd) => execSync(cmd, { stdio: "inherit" });
+
+run("npx astro build");
+run("git checkout --orphan pages-temp");
+run("git rm -rf --cached .");
+run("git add dist/");
+run('git commit -m "deploy :3"');
+run("git push origin pages-temp:pages --force");
+run("git checkout -");
+run("git branch -D pages-temp");
