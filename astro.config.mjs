60816b18afb81f25efec955c92b71f13cd34dfe2
Amelia Rose <nyameliaaaa@proton.me>
2026-05-08 01:29:29 +0300

    oh its not static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,28 @@
 // @ts-check
 
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import { defineConfig } from "astro/config";
-import icon from "astro-icon";
-
-import tailwindcss from "@tailwindcss/vite";
-
-import vue from "@astrojs/vue";
-
-import cloudflare from "@astrojs/cloudflare";
+import cloudflare from '@astrojs/cloudflare';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import vue from '@astrojs/vue';
+import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://nyamelia.is-immensely.gay",
-  integrations: [mdx(), sitemap(), icon(), vue()],
+    site: 'https://nyamelia.is-immensely.gay',
+    integrations: [mdx(), sitemap(), icon(), vue()],
 
-  markdown: {
-      shikiConfig: {
-          theme: "catppuccin-macchiato",
-      },
-  },
+    markdown: {
+        shikiConfig: {
+            theme: 'catppuccin-macchiato'
+        }
+    },
 
-  vite: {
-      plugins: [tailwindcss()],
-  },
+    vite: {
+        plugins: [tailwindcss()]
+    },
 
-  adapter: cloudflare(),
+    output: 'static',
+    adapter: cloudflare()
 });
