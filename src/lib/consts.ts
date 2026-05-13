3c3a2cda286ce1ecddb05f04f0175df4a8ced8b3
Amelia Rose <nyameliaaaa@proton.me>
2026-05-13 20:23:16 +0300

    add commit url

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -2,7 +2,8 @@ export const RSS_TITLE = "amelia's ramblings :3";
 export const RSS_DESCRIPTION =
     'i sometimes bark i mean think i mean bark and i think some people would like to hear that. i think (i dont)';
 
-const isDev = false;
+const currentlyDev = true;
+const isDev = import.meta.env.CF_PAGES_COMMIT_SHA ? false : currentlyDev;
 const prodUrl = 'https://nyamelia-services.nyamelia.workers.dev/api';
 const devUrl = 'http://localhost:8787/api';
 export const WORKERS_URL = isDev ? devUrl : prodUrl;
