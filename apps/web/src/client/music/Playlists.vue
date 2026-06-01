6f43f2a89f4b22968df0b2eaf3eb3b158808176e
Amelia Rose <nyameliaaaa@proton.me>
2026-06-01 10:30:57 +0300

    clean up some stuff

--- apps/web/src/client/music/Playlists.vue
+++ apps/web/src/client/music/Playlists.vue
@@ -7,6 +7,7 @@
 <script setup lang="ts">
 import type { MusicGridItem, Playlist } from '@lib/types';
 import MusicGrid from '@client/components/MusicGrid.vue';
+import { playlists } from '@lib/data';
 
 const migrateDataFormat = (data: Playlist[]): MusicGridItem[] => {
 	return data.map(playlist => ({
@@ -16,73 +17,12 @@ const migrateDataFormat = (data: Playlist[]): MusicGridItem[] => {
 		secondaryText: playlist.content,
 		cardItem: {
 			aria: `${playlist.name} (${playlist.content})`,
-			alt: `${playlist.name} (${playlist.content})`
-		}
+			alt: `${playlist.name} (${playlist.content})`,
+		},
 	}));
 };
 
-const data: Playlist[] = [
-	{
-		name: 'audible gabe',
-		content: 'game OSTs',
-		image: '/playlist-covers/audible-gabe.jpg',
-		url: 'https://music.youtube.com/playlist?list=PLLkb4ldSNGRuD0f0mJk-5JmZv11vRl4aV'
-	},
-	{
-		name: 'there is estrogen in my headphones',
-		content: 'hyperpop',
-		image: '/playlist-covers/estrogen.jpg',
-		url: 'https://music.youtube.com/playlist?list=PLLkb4ldSNGRsA2JV5zAs7VxUc1MfcGoU6'
-	},
-	{
-		name: 'god is dead and i killed him',
-		content: 'heavy metal & friends',
-		image: '/playlist-covers/god-is-dead.jpg',
-		url: 'https://music.youtube.com/playlist?list=PLLkb4ldSNGRvM1vpVWFq6wIbS7dnijIeh'
-	},
-	{
-		name: 'it was a good day (it was not)',
-		content: 'rap',
-		image: '/playlist-covers/good-day.jpg',
-		url: 'https://music.youtube.com/playlist?list=PLLkb4ldSNGRusVfOLoQ3LVNVUZpTZWmIt'
-	},
-	{
-		name: 'mediocre apple',
-		content: 'vocaloid, utaite, jpop & touhou',
-		image: '/playlist-covers/mediocre-apple.jpg',
-		url: 'https://music.youtube.com/playlist?list=PLLkb4ldSNGRv2fTwCQNh6QPIa4NcOROMG'
-	},
-	{
-		name: 'its not a phase mom',
-		content: 'emo, alt rock, grunge & pop punk',
-		image: '/playlist-covers/phase.jpg',
-		url: 'https://music.youtube.com/playlist?list=PLLkb4ldSNGRsRUoal2lP6feuZoT5x1JOf'
-	},
-	{
-		name: 'songs to totally definitely use as a shock collar pattern',
-		content: 'mc, ncs & gd music (affectionate)',
-		image: '/playlist-covers/shock-collar.jpg',
-		url: 'https://music.youtube.com/playlist?list=PLLkb4ldSNGRuYMq_oq3GR7qkjYHdcx434'
-	},
-	{
-		name: "taylor swift's sleep paralysis demons",
-		content: "pop that isn't corporate but not hyper enough for hyperpop",
-		image: '/playlist-covers/taylor-swift.jpg',
-		url: 'https://music.youtube.com/playlist?list=PLLkb4ldSNGRsMUOs9cc3MwXRwskNqLMUG'
-	},
-	{
-		name: 'i have been kidnapped by drunk uzbek space wizards',
-		content: 'folk & power metal',
-		image: '/playlist-covers/uzbek-space-wizard.jpg',
-		url: 'https://music.youtube.com/playlist?list=PLLkb4ldSNGRthbn6YmUznpFHQ3HN2IQfq'
-	},
-	{
-		name: 'yearning :3',
-		content: 'lesbian',
-		image: '/playlist-covers/yearning.jpg',
-		url: 'https://music.youtube.com/playlist?list=PLLkb4ldSNGRspMHSIA0ZpQB7PaONW7yri'
-	}
-].sort((a, b) => {
+const data = playlists.sort((a, b) => {
 	if (b.name > a.name) return -1;
 	if (b.name < a.name) return 1;
 	return 0;
--- apps/web/src/lib/data.ts
+++ apps/web/src/lib/data.ts
@@ -1,4 +1,4 @@
-import type { LinkSection, PixelieEntry, PixelieSection } from '@lib/types';
+import type { LinkEntry, PixelieEntry, PixelieSection, Playlist } from '@lib/types';
 
 export const headerLinks = [
 	{ name: ':3', url: '/' },
@@ -22,43 +22,97 @@ export const footerPixelies: PixelieEntry[] = [
 	// { url: 'https://firefox.com', img: 'https://88x31.nl/gifs/firefox4.gif', alt: 'tested on firefox' }
 ];
 
-
-
-export const homeLinks: LinkSection[] = [
-	{
-		name: 'fediverse',
-		links: [
-			{
-				label: 'mastodon',
-				handle: '@nyameliaaaa@tech.lgbt',
-				url: 'https://tech.lgbt/@nyameliaaaa',
-			},
-			{ label: 'steam', handle: '@nyameliaaaa', url: 'https://steamcommunity.com/id/nyameliaaaa/' },
-		],
-	},
-	{
-		name: 'fiefdoms of palantir',
-		links: [
-			{ label: 'twitter', handle: '@Nyameliaaaa', url: 'https://twitter.com/Nyameliaaaa' },
-			{
-				label: 'bluesky',
-				handle: 'nyamelia.is-immensely.gay',
-				url: 'https://bsky.app/profile/nyamelia.is-immensely.gay',
-			},
-		],
-	},
-	{
-		name: 'scrobblign go brrrr',
-		links: [
-			{ label: 'last.fm', handle: '@nyamelia', url: 'https://last.fm/user/nyamelia' },
-			{ label: 'backloggd', handle: '@nyameliaaaa', url: 'https://backloggd.com/u/nyameliaaaa/' },
-		],
-	},
-	{
-		name: 'purgatory',
-		links: [{ label: 'instagram', handle: '@Nyameliaaaa', url: 'https://instagram.com/Nyameliaaaa' }],
-	},
+export const meLinks: LinkEntry[] = [
+	{ label: 'pronouns.page', handle: '@nyamelia', url: 'https://en.pronouns.page/@Nyameliaaaa' },
+	{ label: 'attraction layer cake', handle: 'rC6', url: 'https://cake.avris.it/rC6' },
+	{ label: 'arvis.it spectrum', handle: 'F4Q2', url: 'https://spectrum.avris.it/F4Q2' },
 ];
+
+export const sourceLinks: LinkEntry[] = [
+	{ label: 'website', handle: 'nyamelia/website', url: 'https://git.gay/nyamelia/website/src/branch/main/apps/web' },
+	{ label: 'helper', handle: 'nyamelia/website', url: 'https://git.gay/nyamelia/website/src/branch/main/apps/worker' },
+];
+
+export const contactLinks: LinkEntry[] = [
+	{
+		label: 'mastodon',
+		handle: '@nyameliaaaa@tech.lgbt',
+		url: 'https://tech.lgbt/@nyameliaaaa',
+	},
+	{ label: 'twitter', handle: '@Nyameliaaaa', url: 'https://twitter.com/Nyameliaaaa' },
+	{
+		label: 'bluesky',
+		handle: '@nyamelia.is-immensely.gay',
+		url: 'https://bsky.app/profile/nyamelia.is-immensely.gay',
+	},
+	{ label: 'instagram', handle: '@Nyameliaaaa', url: 'https://instagram.com/Nyameliaaaa' },
+	{ label: 'steam', handle: '@nyameliaaaa', url: 'https://steamcommunity.com/id/nyameliaaaa/' },
+	{ label: 'last.fm', handle: '@nyamelia', url: 'https://last.fm/user/nyamelia' },
+	{ label: 'backloggd', handle: '@nyameliaaaa', url: 'https://backloggd.com/u/nyameliaaaa/' },
+];
+
+export const playlists: Playlist[] = [
+	{
+		name: 'audible gabe',
+		content: 'game OSTs',
+		image: '/playlist-covers/audible-gabe.jpg',
+		url: 'https://music.youtube.com/playlist?list=PLLkb4ldSNGRuD0f0mJk-5JmZv11vRl4aV',
+	},
+	{
+		name: 'there is estrogen in my headphones',
+		content: 'hyperpop',
+		image: '/playlist-covers/estrogen.jpg',
+		url: 'https://music.youtube.com/playlist?list=PLLkb4ldSNGRsA2JV5zAs7VxUc1MfcGoU6',
+	},
+	{
+		name: 'god is dead and i killed him',
+		content: 'heavy metal & friends',
+		image: '/playlist-covers/god-is-dead.jpg',
+		url: 'https://music.youtube.com/playlist?list=PLLkb4ldSNGRvM1vpVWFq6wIbS7dnijIeh',
+	},
+	{
+		name: 'it was a good day (it was not)',
+		content: 'rap',
+		image: '/playlist-covers/good-day.jpg',
+		url: 'https://music.youtube.com/playlist?list=PLLkb4ldSNGRusVfOLoQ3LVNVUZpTZWmIt',
+	},
+	{
+		name: 'mediocre apple',
+		content: 'vocaloid, utaite, jpop & touhou',
+		image: '/playlist-covers/mediocre-apple.jpg',
+		url: 'https://music.youtube.com/playlist?list=PLLkb4ldSNGRv2fTwCQNh6QPIa4NcOROMG',
+	},
+	{
+		name: 'its not a phase mom',
+		content: 'emo, alt rock, grunge & pop punk',
+		image: '/playlist-covers/phase.jpg',
+		url: 'https://music.youtube.com/playlist?list=PLLkb4ldSNGRsRUoal2lP6feuZoT5x1JOf',
+	},
+	{
+		name: 'songs to totally definitely use as a shock collar pattern',
+		content: 'mc, ncs & gd music (affectionate)',
+		image: '/playlist-covers/shock-collar.jpg',
+		url: 'https://music.youtube.com/playlist?list=PLLkb4ldSNGRuYMq_oq3GR7qkjYHdcx434',
+	},
+	{
+		name: "taylor swift's sleep paralysis demons",
+		content: "pop that isn't corporate but not hyper enough for hyperpop",
+		image: '/playlist-covers/taylor-swift.jpg',
+		url: 'https://music.youtube.com/playlist?list=PLLkb4ldSNGRsMUOs9cc3MwXRwskNqLMUG',
+	},
+	{
+		name: 'i have been kidnapped by drunk uzbek space wizards',
+		content: 'folk & power metal',
+		image: '/playlist-covers/uzbek-space-wizard.jpg',
+		url: 'https://music.youtube.com/playlist?list=PLLkb4ldSNGRthbn6YmUznpFHQ3HN2IQfq',
+	},
+	{
+		name: 'yearning :3',
+		content: 'lesbian',
+		image: '/playlist-covers/yearning.jpg',
+		url: 'https://music.youtube.com/playlist?list=PLLkb4ldSNGRspMHSIA0ZpQB7PaONW7yri',
+	},
+]
 
 export const pixelies = (generator: string): PixelieSection[] => {
 	return [
@@ -98,7 +152,11 @@ export const pixelies = (generator: string): PixelieSection[] => {
 				},
 				{ url: 'https://bitwarden.com', img: 'https://88x31.nl/gifs/bitwarden.gif', alt: 'bitwarden' },
 				{ url: 'https://firefox.com', img: 'https://88x31.nl/gifs/firefox4.gif', alt: 'tested on firefox' },
-				{ url: 'https://qbittorrent.org', img: 'https://sapphic.moe/buttons/other/qbittorrent.png' },
+				{
+					url: 'https://qbittorrent.org',
+					img: 'https://sapphic.moe/buttons/other/qbittorrent.png',
+					alt: 'qbittorrent',
+				},
 			],
 		},
 		{
