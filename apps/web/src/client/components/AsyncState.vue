1fcba3958dff5655e4c714a89026aad72ebf85c0
Amelia Rose <nyameliaaaa@proton.me>
2026-06-01 09:02:33 +0300

    web: reduce layout shifts

--- apps/web/src/client/components/AsyncState.vue
+++ apps/web/src/client/components/AsyncState.vue
@@ -1,44 +1,25 @@
 <template>
-	<TransitionGroup
-		v-if="!static"
-		tag="div"
-		enter-active-class="transition-opacity duration-200"
-		enter-from-class="opacity-0"
-		enter-to-class="opacity-100"
-		leave-active-class="transition-opacity duration-200"
-		leave-from-class="opacity-100"
-		leave-to-class="opacity-0"
-		mode="out-in"
-	>
-		<div v-if="loading" :class="duringBox">
-			<Icon icon="mdi:loading" :class="[body, 'animate-spin']" size="18" />
-			<p :class="body">loading {{ '<3' }}</p>
-		</div>
-		<div v-else-if="error" :class="duringBox">
-			<Icon icon="mdi:alert-circle-outline" :class="body" size="18" />
-			<div :class="body">something went wrong {{ '<\/3' }}</div>
-		</div>
-		<div v-else-if="empty" :class="duringBox">
-			<Icon icon="mdi:cup-outline" :class="body" size="18" />
-			<div :class="body">no entries {{ '<\/3' }}</div>
-		</div>
+	<component :is="static ? 'div' : TransitionGroup" v-bind="static ? {} : transitionProps" tag="div">
+		<slot name="loading" v-if="loading">
+			<div :class="duringBox">
+				<Icon icon="mdi:loading" :class="[body, 'animate-spin']" size="18" />
+				<p :class="body">loading {{ '<3' }}</p>
+			</div>
+		</slot>
+		<slot name="error" v-else-if="error">
+			<div :class="duringBox">
+				<Icon icon="mdi:alert-circle-outline" :class="body" size="18" />
+				<p :class="body">something went wrong {{ '<\/3' }}</p>
+			</div>
+		</slot>
+		<slot name="empty" v-else-if="empty">
+			<div :class="duringBox">
+				<Icon icon="mdi:cup-outline" :class="body" size="18" />
+				<p :class="body">no entries {{ '<\/3' }}</p>
+			</div>
+		</slot>
 		<slot v-else />
-	</TransitionGroup>
-	<div v-else>
-		<div v-if="loading" :class="duringBox">
-			<Icon icon="mdi:loading" :class="[body, 'animate-spin']" size="18" />
-			<p :class="body">loading {{ '<3' }}</p>
-		</div>
-		<div v-else-if="error" :class="duringBox">
-			<Icon icon="mdi:alert-circle-outline" :class="body" size="18" />
-			<div :class="body">something went wrong {{ '<\/3' }}</div>
-		</div>
-		<div v-else-if="empty" :class="duringBox">
-			<Icon icon="mdi:cup-outline" :class="body" size="18" />
-			<div :class="body">no entries {{ '<\/3' }}</div>
-		</div>
-		<slot v-else />
-	</div>
+	</component>
 </template>
 
 <script setup lang="ts">
@@ -52,6 +33,15 @@ defineProps<{
 	empty?: boolean;
 	static?: boolean;
 }>();
+
+const transitionProps = {
+	enterActiveClass: 'transition-opacity duration-200',
+	enterFromClass: 'opacity-0',
+	enterToClass: 'opacity-100',
+	leaveActiveClass: 'transition-opacity duration-200',
+	leaveFromClass: 'opacity-100',
+	leaveToClass: 'opacity-0',
+};
 
 const duringBox =
 	'bg-ctp-crust p-2 border-2 border-dashed border-ctp-base rounded-lg flex flex-row items-center justify-center gap-4';
